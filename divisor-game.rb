5faf0b789fa1668e50132905f2fd2444dbe7ab7e
Khoj Badami <khoj.badami@gmail.com>
2020-09-26 15:19:35 +0530

    1025. Divisor Game

---
+++ divisor-game.rb
@@ -0,0 +1,5 @@
+def divisor_game(n)
+    return n % 2 == 0
+end
+
+// To anybody who sees this. This is a crazy question which I spent days of free time thinking about and writing about. This result works and is 100% correct. But it does not in anyway help to understand why. The why is a result of working out the case of what would happen from n=1 to n=10 manually.
